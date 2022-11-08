df1365a3d144ddc1ef9044e4135e8e2321cd199b
Jsanchez023 <117754676+Jsanchez023@users.noreply.github.com>
2022-11-08 17:09:21 +0100

    Add files via upload

---
+++ Tasca1-ex1.ps1
@@ -0,0 +1,21 @@
+﻿while( 1 -eq 1){ #Crea un bucle per recorrer el programa un alre cop en cas d'error en la ruta
+    clear; $ruta = Read-Host -Prompt 'Introduce la ruta compelta' # demana la ruta per guardarla en  una variable
+    cd \   # estableix la ubicació actual a la arrel
+    cd $ruta -ErrorAction SilentlyContinue  #va a la ruta
+    if($? -eq "true"){  #si la ruta existeix (no dona error la comanda anterior) busca l'arxiu
+        $archivo = Read-Host -Prompt "Introduce el archivo desdeado"  #demana l'arxiu a buscar
+        $resultado = Get-Item -path $ruta"\"$archivo -ErrorAction SilentlyContinue #busca l'arxiu
+        $resultado
+        if($resultado -eq $null){   # si la variable esta vacia no es troba l'arxiu
+        "No se ha encotrado el archivo"  
+        }
+        break
+    }
+    else{
+
+    "Ruta no encotrada."
+    "Introduzca la ruta completa."
+    sleep 2  #espera 2 segons per poder veure el misatje anterior
+
+    }
+}
---
+++ Tasca1-ex2.ps1
@@ -0,0 +1,13 @@
+﻿while( 1 -eq 1){ #Crea un bucle per recorrer el programa un alre cop en cas d'error en la ruta
+clear; $ruta = Read-Host -Prompt 'Introdueix la ruta del arxiu a copiar'  #demana la ruta per guardarla en  una variable
+$copia = Read-Host -Prompt 'Introdueix la ruta on es guardara la copia'   #demana la ruta on copiar l'arxiu i la guarda en una variable
+Copy-Item $ruta -Destination $copia -ErrorAction SilentlyContinue  #copia l'arxiu a la ruta especificada 
+if($? -eq "true"){   # si s'ha realitzat la copia satisfactoriament trenca el bucle
+"S'ha realitzat la copia satisfactoriament"
+break
+}
+else{
+"La ruta del fitxer o del desti no existeix"
+sleep 2  #espera 2 segons per poder veure el misatje anterior
+}
+}
